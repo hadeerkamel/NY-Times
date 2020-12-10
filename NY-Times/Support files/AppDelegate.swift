072139ef0c6748a7cda5277d6a5fc39b10da92a4
Hadeer Kamel <Hadeer_kamel@outlook.com>
2020-12-10 07:28:50 +0200

    Initial Commit

---
+++ NY-Times/Support files/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  NY-Times
+//
+//  Created by Hadeer Kamel on 12/10/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    static var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
